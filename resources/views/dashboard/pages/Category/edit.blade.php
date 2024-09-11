0de7b750b0fa31a563a3ef5f77c84c5323939652
AhmedIT808 <xxahmed20xggx@gmail.com>
2024-09-11 03:04:17 +0300

    Ahmed && Waad

--- resources/views/dashboard/pages/Category/edit.blade.php
+++ resources/views/dashboard/pages/Category/edit.blade.php
@@ -10,8 +10,8 @@
     border-radius: 1.5rem;
     overflow: hidden;
     min-height: 400px;
-    background-color: #eaeaea; لون خلفية فاتح ومناسب للتصميم العصري
-    box-shadow: 0 12px 24px rgba(0, 0, 0, 0.15); /* ظل صندوق أكثر نعومة */
+    background-color: #eaeaea; 
+    box-shadow: 0 12px 24px rgba(0, 0, 0, 0.15); 
 }
 
 .card-custom {
@@ -23,7 +23,7 @@
 }
 
 .card-custom:hover {
-    /* background: linear-gradient(135deg, #e45c6c, #c87ef6); تدرج ألوان أعمق عند التمرير */
+
     transform: translateY(-8px);
     box-shadow: 0 20px 30px rgba(0, 0, 0, 0.25);
 }
@@ -41,25 +41,51 @@
 .btn-custom {
     border-radius: 30px;
     font-size: 1rem;
-    padding: 12px 28px;
+    padding: 12px 24px;
     transition: all 0.3s ease;
-    border: none;
-    background-color:  #fff; 
-    color: #000;
-   
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.15);
+    background-color:#E4E7EC;
+    
 }
 
-.btn-custom:hover, .btn-custom:focus {
-    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-    outline: none;
-    text-decoration: none;
-    background-color:#DC3545 ;
+.btn-custom:hover {
+    transform: scale(1.05);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    background-color:#198754;
     color: #fff;
 }
 
+.btn-custom-reset {
+    border-radius: 30px;
+    font-size: 1rem;
+    padding: 12px 24px;
+    transition: all 0.3s ease;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.15);
+    background-color:#E4E7EC;
+}
 
+.btn-custom-reset:hover {
+    transform: scale(1.05);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    background-color:#DC3545;
+    color: #fff;
+}
 
+.btn-custom-go {
+    border-radius: 30px;
+    font-size: 1rem;
+    padding: 12px 24px;
+    transition: all 0.3s ease;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.15);
+    background-color:#E4E7EC;
+}
 
+.btn-custom-go:hover {
+    transform: scale(1.05);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    background-color:#6C757D;
+    color: #fff;
+}
 
 .card-title {
     font-weight: 700;
@@ -77,13 +103,10 @@
 <div class="container container-custom w-50 m-auto my-3">
     <div class="row justify-content-center">
         <div class="col-12">
-<<<<<<< HEAD
-            <div class="card card-custom shadow-md mb-4">
-                <strong class="card-title fs-2">{{ __('edit--.heading', ['category' => $category->title]) }}</strong>
-=======
+
             <div class="card shadow-md mb-4">
                 <strong class="card-title fs-2">{{ __('edit-dash.heading', ['category' => $category->title]) }}</strong>
->>>>>>> 64922bd68c6ec449d61e9cfbeb37817a927015df
+
             </div>
             <div class="card-body card-body-custom">
                 <div class="row">
@@ -92,15 +115,11 @@
                             @csrf
                             @method('PUT')
                             @include('dashboard.pages.category.form')
-<<<<<<< HEAD
-                            <button type="submit" class="btn  btn-custom btn-md px-4 font-weight-bold fs-5">{{ __('edit--.update_button') }}</button>
-                            <a href="{{ route('categories.index') }}" class="btn  btn-custom btn-md px-2 py-2">{{ __('edit--.return_button') }}</a>
-                            <a href="{{ url()->previous() }}" class="btn  btn-custom btn-md px-2 py-2">{{ __('edit--.back_button') }}</a>
-=======
-                            <button type="submit" class="btn btn-primary btn-md px-4 font-weight-bold fs-5">{{ __('edit-dash.update_button') }}</button>
-                            <a href="{{ route('categories.index') }}" class="btn btn-dark btn-md px-2 py-2">{{ __('edit-dash.return_button') }}</a>
-                            <a href="{{ url()->previous() }}" class="btn btn-dark btn-md px-2 py-2">{{ __('edit-dash.back_button') }}</a>
->>>>>>> 64922bd68c6ec449d61e9cfbeb37817a927015df
+
+                            <button type="submit" class="btn       btn-md px-4 font-weight-bold fs-5  btn-custom">{{ __('edit-dash.update_button') }}</button>
+                            <a href="{{ route('categories.index') }}" class="btn  btn-md px-2 py-2  btn-custom-reset">{{ __('edit-dash.return_button') }}</a>
+                            <a href="{{ url()->previous() }}" class="btn  btn-md px-2 py-2  btn-custom-go">{{ __('edit-dash.back_button') }}</a>
+
                         </form>
                     </div>
                 </div>
